7e64f782f5680fdcaa079aa3f2475e5817ccf33a
yoonucho <tomorrowcho@gmail.com>
2025-09-30 23:14:48 +0900

    feat: 전역 프로바이더 설정
    - TanStack Query, Zustand, Supabase 클라이언트를 위한 프로바이더 구현
    
    - AppProviders를 통해 모든 프로바이더를 통합 관리
    
    - 루트 레이아웃에 프로바이더를 적용하여 앱 전체에서 상태 관리 및 데이터 페칭이 가능하도록 설정

---
+++ src/app/providers/index.ts
@@ -0,0 +1,8 @@
+// Providers - 애플리케이션 프로바이더들
+// React Context와 전역 설정을 위한 프로바이더들을 export
+
+// UI 컴포넌트 레이어
+export { default as QueryProvider } from "./QueryProvider";
+export { default as SupabaseProvider } from "./SupabaseProvider";
+export { default as ThemeProvider } from "./ThemeProvider";
+export { default as AppProviders } from "./AppProviders";

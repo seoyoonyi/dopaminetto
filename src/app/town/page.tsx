d337cde9b442febe58e4d1944e6fb35449c9ff79
seoyoonyi <yiseoyoon@gmail.com>
2025-11-05 21:59:31 +0900

    feat: town 페이지에 환영 메시지 추가 (#11)

--- src/app/town/page.tsx
+++ src/app/town/page.tsx
@@ -1,5 +1,15 @@
 "use client";
 
-export default function GamePage() {
-  return <div>Game Page</div>;
+import { useUserStore } from "@/shared/store";
+
+export default function TownPage() {
+  const { userNickname } = useUserStore();
+
+  return (
+    <>
+      <h1 className="text-xl p-6">
+        환영합니다, <span className="font-display font-bold">{userNickname}</span>님!
+      </h1>
+    </>
+  );
 }

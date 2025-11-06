3ff30c21d06644e135d5beaffbb1dc7346c28aee
seoyoonyi <yiseoyoon@gmail.com>
2025-11-06 16:04:01 +0900

    feat: town 페이지 레이아웃 구성 (#11)

--- src/app/town/page.tsx
+++ src/app/town/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { ChatHistory } from "@/features/chat";
 import { useUserStore } from "@/shared/store";
 
 export default function TownPage() {
   const { userNickname } = useUserStore();
 
   return (
-    <>
-      <h1 className="text-xl p-6">
-        환영합니다, <span className="font-display font-bold">{userNickname}</span>님!
-      </h1>
-    </>
+    <div className="flex">
+      {/*  Map 컴포넌트가 들어올 예정 */}
+      <div className="flex-1"></div>
+
+      <ChatHistory userNickname={userNickname} />
+    </div>
   );
 }

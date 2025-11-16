7d3df92945065922cc3174c185a5013fdd88a736
tomorrowcho <tomorrowcho@gmail.com>
2025-11-17 03:13:18 +0900

    docs: Presence 스토어 lastSyncedAt 유지 이유 주석 추가

--- src/features/presence/model/useTownPresenceStore.ts
+++ src/features/presence/model/useTownPresenceStore.ts
@@ -5,6 +5,10 @@ import { PresenceParticipant } from "../types";
 interface TownPresenceState {
   participants: PresenceParticipant[];
   isConnected: boolean;
+  /**
+   * Presence 목록이 마지막으로 갱신된 시각
+   * 향후 UI에서 최근 동기화 정보를 표시할 때 활용 예정
+   */
   lastSyncedAt?: string;
   setParticipants: (participants: PresenceParticipant[]) => void;
   setConnectionState: (isConnected: boolean) => void;

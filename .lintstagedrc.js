d68a18fab06d4d01090800490596b81433743c98
seoyoonyi <yiseoyoon@gmail.com>
2025-11-17 20:00:03 +0900

    chore: tsc --noEmit 타입체크 추가 (#32)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,11 @@
-// .lintstagedrc.js
-
 module.exports = {
-  // TypeScript, JavaScript 파일에 대해 Prettier 포맷팅과 ESLint 검사를 실행
-  "**/*.{ts,tsx,js,jsx}": ["prettier --write", "eslint --fix"],
+  // TypeScript, JavaScript 파일에 대해 Prettier 포맷팅, ESLint 검사, 타입 체크를 실행
+  "**/*.{ts,tsx}": [
+    "prettier --write",
+    "eslint --fix",
+    () => "tsc --noEmit", // 타입 체크 (staged 파일뿐만 아니라 전체 프로젝트)
+  ],
+  "**/*.{js,jsx}": ["prettier --write", "eslint --fix"],
   // 기타 파일(JSON, Markdown 등)에 대해서는 Prettier 포맷팅만 실행
   "**/*.{json,md,yml}": ["prettier --write"],
 };

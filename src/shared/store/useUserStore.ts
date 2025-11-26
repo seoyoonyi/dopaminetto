555d5c02a122ed11f499c1378ae47331fffb2d3e
seoyoonyi <yiseoyoon@gmail.com>
2025-11-26 15:06:42 +0900

    feat: useUserStore에 userId 추가  (#50)

--- src/shared/store/useUserStore.ts
+++ src/shared/store/useUserStore.ts
@@ -1,16 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface UserStoreState {
+  userId: string;
   userNickname: string;
   setUserNickname: (nickname: string) => void;
+  reset: () => void;
 }
 
 export const useUserStore = create<UserStoreState>()(
   persist(
-    (set) => ({
+    (set, get) => ({
+      userId: "",
       userNickname: "",
-      setUserNickname: (nickname) => set({ userNickname: nickname }),
+      setUserNickname: (nickname) => {
+        const currentUserId = get().userId;
+        set({
+          userNickname: nickname,
+          userId: currentUserId || uuidv4(),
+        });
+      },
+      reset: () => set({ userId: "", userNickname: "" }),
     }),
     {
       name: "user-storage",

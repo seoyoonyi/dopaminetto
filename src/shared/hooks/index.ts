0a85e2cc16cb3cb27880559b42a6fd995fb07a15
tomorrowcho <tomorrowcho@gmail.com>
2026-02-03 01:59:32 +0900

    docs: hooks/index.ts 주석을 실제 내용에 맞게 수정

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,5 +1,5 @@
-// Hooks - 공통 훅들
-// 재사용 가능한 React 훅들을 export
+// Hooks - 공통 및 기능별 훅들
+// 공통적으로 사용하는 React 훅들과 일부 기능별 훅들을 export
 export * from "./useUserInfo";
 export * from "./useIntersectionObserver";
 export * from "./useVisiblePageTracking";
